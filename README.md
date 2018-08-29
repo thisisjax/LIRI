0d7af71b6227adc240fda0dbab2589ef3c0f4783
Jacqueline Demers <jacquelinedemers@Jacquelines-MacBook-Pro.local>
2018-08-29 09:57:49 -0400

    final push

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 node_modules
 .DS_Store
 .env
+
+
+Video to my LIRI BOT:
+
+https://drive.google.com/file/d/19J-mw1WThwSPvKTRaWAkAbF0dTCzj5vT/view
+
+![my attempted code for  the moviethis:](/images/moviethiscode.png)
--- liri.js
+++ liri.js
@@ -10,103 +10,110 @@ var request = require("request");
 
 var movieName = process.argv[3];
 var liriAnswer = process.argv[2];
-
 var Spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 //Commands for Liri
 switch (liriAnswer) {
     case "my-tweets":
-    myTweets();
-    break;
+        myTweets();
+        break;
 
     case "spotify-this-song":
-    spotifyThisSong();
-    break;
+        spotifyThisSong();
+        break;
 
     case "movie-this":
-    movieThis();
-    break;
+        movieThis();
+        break;
 
     case "do-what-it-says":
-    doWhatItSays();
-    break;
+        doWhatItSays();
+        break;
 
 };
 
 //Command (my-tweets)
-    function myTweets(){
-        var tweet = { screen_name: "Jackie94573448" };
-        client.get("statuses/user_timeline", tweet, function (error, tweets, response) {
-            if(!error) {
-                for (var i = 0; i < tweets.length; i++) {
-                    console.log(tweets[i].text);
-                };
-            } else {
-                console.log("error: " + err);
-                return;
+function myTweets() {
+    var tweet = { screen_name: "Jackie94573448" };
+    client.get("statuses/user_timeline", tweet, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].text);
             };
-        
-        });
-    };
-
-//Command (spotify-this-song)
-function spotifyThisSong(trackName) {
-    var trackName = process.argv[3];
-    if (!trackName) {
-        trackName = "The Sign";
-    }
-    songRequest = trackName;
-    spotify.search({
-        type: "track",
-        query: songRequest
-    },
-
-  
-    function (err, data) {
-        if(!err) {
-            var trackResponse = data.tracks.items;
-            for (var i = 0; i < 5; i++) {
-                if (trackResponse[i] != undefined) {
-                    var spotifyOutput =
-                    "Artist: " + trackResponse[i].artist[0].name + "\n" +
-                    "Song: " + trackResponse[i].name + "\n" +
-                    "Preview Link: " + trackResponse[i].preview_url + "\n" +
-                    "Album: " + trackResponse[i].album.name + "\n"
-            console.log(spotifyOutput);
-            console.log(" ");
-                }
-            }
         } else {
             console.log("error: " + err);
             return;
         };
+
     });
 };
 
-//Command (movie-this)
-function movieThis(){
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=6547b013";
 
-    request(queryUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var movieData = JSON.parse(body);
-            var queryUrlResults =
-            "Title: " + movieData.Title + "\n" +
-            "Year: " + movieData.Year + "\n" +
-            "IMDB Rating: " + movieData.Ratings[0].Value + "\n" +
-            "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value + "\n" +
-            "Origin Country: " + movieData.Country + "\n" +
-            "Language: " + movieData.Language + "\n" +
-            "Plot: " + movieData.Plot + "\n" +
-            "Actors: " + movieData.Actors + "\n"
+//Command (spotify-this-song)
+function spotifyThisSong(trackName) {
 
-        console.log(queryUrlResults);
+    var trackName = process.argv[3];
 
-        } else {
-                console.log()
+    if (!trackName) {
+        trackName = "The Sign";
+    }
+
+    songRequest = trackName;
+    Spotify.search({ type: "track", query: songRequest }, function (err, data) {
+        // console.log(data.tracks.items[0].album.name)
+
+
+        if (!err) {
+            var trackResponse = data.tracks.items;
+
+
+
+            for (var i = 0; i < 5; i++) {
+                if (trackResponse[i] != undefined) {
+
+
+
+                    var spotifyOutput = 
+                        "Artist: " + trackResponse[i].album.artists[0].name + "\n" + 
+                        "Song: " + trackResponse[i].name + "\n" +
+                        "Preview Link: " + trackResponse[i].preview_url + "\n" +
+                        "Album: " + trackResponse[i].album.name + "\n"
+                    console.log(spotifyOutput);
+
+
+
+                } else {
+                    console.log("error: " + err);
+                    return;
+                }
+            };
+        };
+
+        //Command (movie-this)
+        function movieThis() {
+            var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=6547b013";
+
+            request(queryUrl, function (error, response, body) {
+                if (!error && response.statusCode === 200) {
+                    var movieData = JSON.parse(body);
+                    var queryUrlResults =
+                        "Title: " + movieData.Title + "\n" +
+                        "Year: " + movieData.Year + "\n" +
+                        "IMDB Rating: " + movieData.Ratings[0].Value + "\n" +
+                        "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value + "\n" +
+                        "Origin Country: " + movieData.Country + "\n" +
+                        "Language: " + movieData.Language + "\n" +
+                        "Plot: " + movieData.Plot + "\n" +
+                        "Actors: " + movieData.Actors + "\n"
+
+                    console.log(queryUrlResults);
+
+                } else {
+                    console.log()
+                }
+            });
         }
-    })
-    
+    }
+    )
 }
-
